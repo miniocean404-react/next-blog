3e0e09af4244b5d8e1c677b336f9bba583fd3ba9
MiniOcean404 <1037306928@qq.com>
2022-04-17 23:46:08 +0800

    style: 添加提交消息正则的案例注释

--- script/verify-commit.js
+++ script/verify-commit.js
@@ -1,7 +1,8 @@
 // Invoked on the commit-msg git hook by yorkie.
 
 const chalk = require('chalk')
-const msgPath = process.env.GIT_PARAMS
+// Yorkie的gitHooks使用process.env.GIT_PARAMS
+const msgPath = process.argv[2]
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
 const commitRE =
@@ -12,14 +13,12 @@ if (!commitRE.test(msg)) {
 	if (!mergeRe.test(msg)) {
 		console.log(msg)
 		console.error(
-			`  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
-				chalk.red(
-					`  Proper commit message format is required for automated changelog generation. Examples:\n\n`
-				) +
-				`    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+			`  ${chalk.bgRed.white(' 错误 ')} ${chalk.red(`错误的消息格式`)}\n\n` +
+				chalk.red(`  自动化校验-提交模板样例:\n\n`) +
+				`    ${chalk.green(`feat(compiler): add 'comments' option（冒号后有空格）`)}\n` +
 				`    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
 				chalk.red(
-					`  See https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.\n`
+					`  查看文档详情：https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.\n`
 				)
 		)
 		process.exit(1)
