8747b9431dbe78d8ff4a8b95e04306e60a2c4932
MiniOcean404 <1037306928@qq.com>
2022-04-17 23:44:19 +0800

    style(githook): 提交格式校验

--- script/verify-commit.js
+++ script/verify-commit.js
@@ -1,6 +1,6 @@
 // Invoked on the commit-msg git hook by yorkie.
 
-import chalk from 'chalk'
+const chalk = require('chalk')
 const msgPath = process.env.GIT_PARAMS
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
