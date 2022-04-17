e1a2be396e493baca05cf876f8dcfb67371cfe14
MiniOcean404 <1037306928@qq.com>
2022-04-17 22:46:39 +0800

    style:添加配置

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = (phase, { defaultConfig }) => {
+	if (phase === PHASE_DEVELOPMENT_SERVER) {
+		return {
+			/* development only config options here */
+		}
+	}
+
+	return {
+		reactStrictMode: true,
+		basePath: '/forum', // 路由前缀
+		env: {
+			customKey: 'value',
+		},
+
+		compress: true, // Next.js 提供gzip压缩来压缩渲染的内容和静态文件
+	}
+}
