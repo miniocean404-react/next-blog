7d6e68f957ba27294f695858c23773d7bb50cd6f
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-10-14 17:01:34 +0800

    ✨ feat: 布局

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,26 +5,9 @@ import localFont from "next/font/local"
 import { getMessages } from "next-intl/server"
 import { NextIntlClientProvider } from "next-intl"
 
-// 使用next/font来加载谷歌字体，而不是在css到声明字体，因为它帮我们优化了字体的加载，很方便使用各种各样的字体
-const miSansFont = localFont({
-  src: "../../assets/font/MiSans VF.ttf",
-  // 就是 css font-display
-  display: "swap",
-  weight: "400",
-  variable: "--MiSans",
-})
-
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
 export default async function LocaleLayout({ children, params: { locale } }: { children: React.ReactNode; params: { locale: string } }) {
   const messages = await getMessages({ locale })
-
-  return (
-    // suppressHydrationWarning：关闭水合不匹配报错
-    <html lang={locale} suppressHydrationWarning>
-      <body className={miSansFont.className}>
-        <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
-      </body>
-    </html>
-  )
+  return <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,16 @@ import ReactLenis from "lenis/react"
 import { ThemeProvider } from "next-themes"
 import { APP_DEFAULT_TITLE, APP_DESCRIPTION, APP_NAME, APP_TITLE_TEMPLATE } from "@/constant/app"
 import type { Metadata } from "next"
+import localFont from "next/font/local"
+
+// 使用next/font来加载谷歌字体，而不是在css到声明字体，因为它帮我们优化了字体的加载，很方便使用各种各样的字体
+const miSansFont = localFont({
+  src: "../assets/font/MiSans VF.ttf",
+  // 就是 css font-display
+  display: "swap",
+  weight: "400",
+  variable: "--MiSans",
+})
 
 // 在页面中也可以设置专属页面 metadata，并与顶级 metadata 进行merge
 export const metadata: Metadata = {
@@ -51,8 +61,9 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children, params: { locale } }: { children: React.ReactNode; params: { locale: string } }) {
   return (
-    <html>
-      <body>
+    // suppressHydrationWarning：关闭水合不匹配报错
+    <html suppressHydrationWarning>
+      <body className={miSansFont.className}>
         {/* 切换：const { theme, setTheme } = useTheme(); */}
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <ReactLenis root options={{ gestureOrientation: "both" }}>
