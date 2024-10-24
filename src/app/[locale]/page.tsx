ae482f5100951d568a6b57b42fe6d2979bbebfec
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-10-24 17:16:48 +0800

    ✨ feat: 样式修改

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,51 +3,9 @@
 import PixiScreen from "@/components/pixi-screen"
 import { startConfetti } from "@/utils/confetti"
 import { useEffect, useRef } from "react"
-import styles from "./index.module.scss"
 import Typed from "typed.js"
 import Particles from "~/lib/components/particles"
-import Marquee from "~/lib/components/marquee"
-import HalfOverlay from "~/lib/components/half-overlay"
-import HeroVideoDialog from "~/lib/components/hero-video-dialog"
-
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-]
+import { cn } from "@/utils/tw"
 
 export default function Home(props: { params: { locale: string } }) {
   const el = useRef(null)
@@ -64,22 +22,14 @@ export default function Home(props: { params: { locale: string } }) {
   }, [])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <h1 ref={el}></h1>
+    <div>
+      <div className={"h-screen flex justify-center items-center m-auto text-6xl"}>
+        <h1 className="text-6xl" ref={el}></h1>
       </div>
 
       {/* <p>用艺术家的视角审视，以工匠精神创造开发</p> */}
       {/* <PixiScreen></PixiScreen> */}
-      <Particles className={styles.particles} quantity={100} ease={80} color={"#000000"} refresh></Particles>
-
-      <HeroVideoDialog
-        className="dark:hidden block"
-        animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
-        thumbnailAlt="Hero Video"
-      />
+      <Particles className={"fixed inset-0 [z-index:var(--vp-z-index-bg)]"} quantity={100} ease={80} color={"#000000"} refresh></Particles>
     </div>
   )
 }
