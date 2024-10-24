b54740fc49b22150194bc65b8e1d32f757a6184c
MiniOcean404 <1037306928@qq.com>
2024-10-25 01:03:13 +0800

    ✨ feat: google

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,19 +4,19 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://acme.com",
+      url: "blog-miniocean404s.vercel.app",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://acme.com/about",
+      url: "blog-miniocean404s.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://acme.com/blog",
+      url: "blog-miniocean404s.vercel.app/zh",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
