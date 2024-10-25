064b104965e193ed650fd03b5b2f0e93a9dec755
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-10-25 14:25:03 +0800

    ✨ feat: ui

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,9 @@
 "use client"
 
-import PixiScreen from "@/components/pixi-screen"
 import { startConfetti } from "@/utils/confetti"
 import { useEffect, useRef } from "react"
 import Typed from "typed.js"
-import Particles from "~/lib/components/particles"
+import Particles from "~/lib/components/magicui/particles"
 
 export default function Home(props: { params: { locale: string } }) {
   const el = useRef(null)
@@ -27,7 +26,6 @@ export default function Home(props: { params: { locale: string } }) {
       </div>
 
       {/* <p>用艺术家的视角审视，以工匠精神创造开发</p> */}
-      {/* <PixiScreen></PixiScreen> */}
       <Particles className={"fixed inset-0 [z-index:var(--vp-z-index-bg)]"} quantity={100} ease={80} color={"#000000"} refresh></Particles>
     </div>
   )
