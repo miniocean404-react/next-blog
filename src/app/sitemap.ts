cd3ab5eda9ece54af616bc90dad519fc87d54a25
MiniOcean404 <1037306928@qq.com>
2024-10-26 20:51:49 +0800

    ✨ feat: vercel seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,33 @@
 import { MetadataRoute } from "next"
 
+const url = "https://blog-miniocean404s.vercel.app"
+
 // 站点地图 可帮助搜索引擎更有效地发现您的网页并为其建立索引，也分为动态和静态两种
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://blog-miniocean404s.vercel.app",
-      lastModified: new Date(),
+      url,
+      lastModified: new Date().toISOString(),
       changeFrequency: "yearly",
       priority: 1,
     },
-    // {
-    //   url: "https://blog-miniocean404s.vercel.app/",
-    //   lastModified: new Date(),
-    //   changeFrequency: "monthly",
-    //   priority: 0.8,
-    // },
     {
-      url: "https://blog-miniocean404s.vercel.app/zh",
-      lastModified: new Date(),
+      url: `${url}/zh`,
+      lastModified: new Date().toISOString(),
       changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 1,
+    },
+    {
+      url: `${url}/manifest.webmanifest`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${url}/robots.txt`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ]
 }
