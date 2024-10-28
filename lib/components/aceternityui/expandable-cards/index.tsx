80b5ae16d8fe43b557309a87d2c96ee35f5e4649
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-10-28 14:51:16 +0800

    ✨ feat: card

--- lib/components/aceternityui/expandable-cards/index.tsx
+++ lib/components/aceternityui/expandable-cards/index.tsx
@@ -32,9 +32,15 @@ export function ExpandableCardDemo() {
     <>
       <AnimatePresence>
         {active && typeof active === "object" && (
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="fixed inset-0 bg-black/20 h-full w-full z-10" />
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-black/20 h-full w-full z-10"
+          />
         )}
       </AnimatePresence>
+
       <AnimatePresence>
         {active && typeof active === "object" ? (
           <div className="fixed inset-0  grid place-items-center z-[100]">
@@ -77,10 +83,16 @@ export function ExpandableCardDemo() {
               <div>
                 <div className="flex justify-between items-start p-4">
                   <div className="">
-                    <motion.h3 layoutId={`title-${active.title}-${id}`} className="font-bold text-neutral-700 dark:text-neutral-200">
+                    <motion.h3
+                      layoutId={`title-${active.title}-${id}`}
+                      className="font-bold text-neutral-700 dark:text-neutral-200"
+                    >
                       {active.title}
                     </motion.h3>
-                    <motion.p layoutId={`description-${active.description}-${id}`} className="text-neutral-600 dark:text-neutral-400">
+                    <motion.p
+                      layoutId={`description-${active.description}-${id}`}
+                      className="text-neutral-600 dark:text-neutral-400"
+                    >
                       {active.description}
                     </motion.p>
                   </div>
@@ -110,6 +122,7 @@ export function ExpandableCardDemo() {
           </div>
         ) : null}
       </AnimatePresence>
+
       <ul className="max-w-2xl mx-auto w-full gap-4">
         {cards.map((card, index) => (
           <motion.div
@@ -120,13 +133,25 @@ export function ExpandableCardDemo() {
           >
             <div className="flex gap-4 flex-col md:flex-row ">
               <motion.div layoutId={`image-${card.title}-${id}`}>
-                <Image width={100} height={100} src={card.src} alt={card.title} className="h-40 w-40 md:h-14 md:w-14 rounded-lg object-cover object-top" />
+                <Image
+                  width={100}
+                  height={100}
+                  src={card.src}
+                  alt={card.title}
+                  className="h-40 w-40 md:h-14 md:w-14 rounded-lg object-cover object-top"
+                />
               </motion.div>
               <div className="">
-                <motion.h3 layoutId={`title-${card.title}-${id}`} className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left">
+                <motion.h3
+                  layoutId={`title-${card.title}-${id}`}
+                  className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left"
+                >
                   {card.title}
                 </motion.h3>
-                <motion.p layoutId={`description-${card.description}-${id}`} className="text-neutral-600 dark:text-neutral-400 text-center md:text-left">
+                <motion.p
+                  layoutId={`description-${card.description}-${id}`}
+                  className="text-neutral-600 dark:text-neutral-400 text-center md:text-left"
+                >
                   {card.description}
                 </motion.p>
               </div>
@@ -187,11 +212,14 @@ const cards = [
     content: () => {
       return (
         <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for her melancholic and cinematic music style. Born Elizabeth Woolridge Grant in New
-          York City, she has captivated audiences worldwide with her haunting voice and introspective lyrics. <br /> <br /> Her songs often explore themes of
-          tragic romance, glamour, and melancholia, drawing inspiration from both contemporary and vintage pop culture. With a career that has seen numerous
-          critically acclaimed albums, Lana Del Rey has established herself as a unique and influential figure in the music industry, earning a dedicated fan
-          base and numerous accolades.
+          Lana Del Rey, an iconic American singer-songwriter, is celebrated for her melancholic and
+          cinematic music style. Born Elizabeth Woolridge Grant in New York City, she has captivated
+          audiences worldwide with her haunting voice and introspective lyrics. <br /> <br /> Her
+          songs often explore themes of tragic romance, glamour, and melancholia, drawing
+          inspiration from both contemporary and vintage pop culture. With a career that has seen
+          numerous critically acclaimed albums, Lana Del Rey has established herself as a unique and
+          influential figure in the music industry, earning a dedicated fan base and numerous
+          accolades.
         </p>
       )
     },
@@ -205,10 +233,13 @@ const cards = [
     content: () => {
       return (
         <p>
-          Babu Maan, a legendary Punjabi singer, is renowned for his soulful voice and profound lyrics that resonate deeply with his audience. Born in the
-          village of Khant Maanpur in Punjab, India, he has become a cultural icon in the Punjabi music industry. <br /> <br /> His songs often reflect the
-          struggles and triumphs of everyday life, capturing the essence of Punjabi culture and traditions. With a career spanning over two decades, Babu Maan
-          has released numerous hit albums and singles that have garnered him a massive fan following both in India and abroad.
+          Babu Maan, a legendary Punjabi singer, is renowned for his soulful voice and profound
+          lyrics that resonate deeply with his audience. Born in the village of Khant Maanpur in
+          Punjab, India, he has become a cultural icon in the Punjabi music industry. <br /> <br />{" "}
+          His songs often reflect the struggles and triumphs of everyday life, capturing the essence
+          of Punjabi culture and traditions. With a career spanning over two decades, Babu Maan has
+          released numerous hit albums and singles that have garnered him a massive fan following
+          both in India and abroad.
         </p>
       )
     },
@@ -223,10 +254,13 @@ const cards = [
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their powerful sound and intense performances that resonate deeply with their
-          audience. Formed in Los Angeles, California, they have become a cultural icon in the heavy metal music industry. <br /> <br /> Their songs often
-          reflect themes of aggression, social issues, and personal struggles, capturing the essence of the heavy metal genre. With a career spanning over four
-          decades, Metallica has released numerous hit albums and singles that have garnered them a massive fan following both in the United States and abroad.
+          Metallica, an iconic American heavy metal band, is renowned for their powerful sound and
+          intense performances that resonate deeply with their audience. Formed in Los Angeles,
+          California, they have become a cultural icon in the heavy metal music industry. <br />{" "}
+          <br /> Their songs often reflect themes of aggression, social issues, and personal
+          struggles, capturing the essence of the heavy metal genre. With a career spanning over
+          four decades, Metallica has released numerous hit albums and singles that have garnered
+          them a massive fan following both in the United States and abroad.
         </p>
       )
     },
@@ -240,10 +274,12 @@ const cards = [
     content: () => {
       return (
         <p>
-          Led Zeppelin, a legendary British rock band, is renowned for their innovative sound and profound impact on the music industry. Formed in London in
-          1968, they have become a cultural icon in the rock music world. <br /> <br /> Their songs often reflect a blend of blues, hard rock, and folk music,
-          capturing the essence of the 1970s rock era. With a career spanning over a decade, Led Zeppelin has released numerous hit albums and singles that have
-          garnered them a massive fan following both in the United Kingdom and abroad.
+          Led Zeppelin, a legendary British rock band, is renowned for their innovative sound and
+          profound impact on the music industry. Formed in London in 1968, they have become a
+          cultural icon in the rock music world. <br /> <br /> Their songs often reflect a blend of
+          blues, hard rock, and folk music, capturing the essence of the 1970s rock era. With a
+          career spanning over a decade, Led Zeppelin has released numerous hit albums and singles
+          that have garnered them a massive fan following both in the United Kingdom and abroad.
         </p>
       )
     },
@@ -257,10 +293,13 @@ const cards = [
     content: () => {
       return (
         <p>
-          &quot;Aawarapan&quot;, a Bollywood movie starring Emraan Hashmi, is renowned for its intense storyline and powerful performances. Directed by Mohit
-          Suri, the film has become a significant work in the Indian film industry. <br /> <br /> The movie explores themes of love, redemption, and sacrifice,
-          capturing the essence of human emotions and relationships. With a gripping narrative and memorable music, &quot;Aawarapan&quot; has garnered a massive
-          fan following both in India and abroad, solidifying Emraan Hashmi&apos;s status as a versatile actor.
+          &quot;Aawarapan&quot;, a Bollywood movie starring Emraan Hashmi, is renowned for its
+          intense storyline and powerful performances. Directed by Mohit Suri, the film has become a
+          significant work in the Indian film industry. <br /> <br /> The movie explores themes of
+          love, redemption, and sacrifice, capturing the essence of human emotions and
+          relationships. With a gripping narrative and memorable music, &quot;Aawarapan&quot; has
+          garnered a massive fan following both in India and abroad, solidifying Emraan
+          Hashmi&apos;s status as a versatile actor.
         </p>
       )
     },
