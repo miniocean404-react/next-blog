01b1b98b8f8e7fafc6062ef924ec7359d36a5756
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-10-30 16:12:50 +0800

    ✨ feat: search

--- src/components/header/client.tsx
+++ src/components/header/client.tsx
@@ -11,6 +11,9 @@ import { SiGithub, SiNotion } from "@icons-pack/react-simple-icons"
 import { cn } from "@/utils/tw"
 import { GITHUB_LINK, NOTION_LINK } from "@/constant/link"
 import ThemeSwitch from "@/components/header/theme-switch"
+import { DocSearch } from "@docsearch/react"
+import "@docsearch/css"
+import { zhTranslations } from "@/constant/docsearch"
 
 export default function HeaderClient({ children, os }: PropsWithChildren<any>) {
   const t = useTranslations("")
@@ -170,7 +173,21 @@ export default function HeaderClient({ children, os }: PropsWithChildren<any>) {
         </div>
       </header>
 
+      {/* 彩虹 */}
       <div className="w-full h-24 fixed top-[0] pointer-events-none before:content-[''] before:absolute before:w-full before:h-3/5 before:z-0 before:left-2/4 before:top-[0] before:-bottom-1/5 before:-translate-x-1/2 before:translate-y-[0] before:rotate-[0] before:skew-x-[0] before:skew-y-[0] before:scale-x-100 before:scale-y-100 before:filter blur-3xl before:opacity-20 before:[background-size:200%] before:bg-[linear-gradient(90deg,_#ff4242,_#a1ff42,_#42a1ff,_#42d0ff,_#a142ff)] before:animate-[rainbow_var(--speed,_2s)_infinite_linear] z-[var(--vp-z-index-header)]"></div>
+
+      {/* <link rel="preconnect" href="https://YOUR_APP_ID-dsn.algolia.net" crossorigin /> */}
+      <DocSearch
+        appId={"R2IYF7ETH7"}
+        apiKey={"599cec31baffa4868cae4e79f180729b"}
+        indexName={"docsearch"}
+        searchParameters={{
+          facetFilters: [`language:${locale}`, "version:1.0.0"],
+        }}
+        insights
+        initialQuery="第一次查询参数"
+        translations={zhTranslations}
+      />
     </>
   )
 }
