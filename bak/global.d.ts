26518bcc85e26848981840e38b6f5d1c13b8fff8
MiniOcean404 <1037306928@qq.com>
2024-10-31 01:06:33 +0800

    ✨ feat: ts

--- bak/global.d.ts
+++ bak/global.d.ts
@@ -1,132 +1,4 @@
 import auth from "next-auth"
-import {
-  useTranslations,
-  ReactNode,
-  Formats,
-  TranslationValues,
-  MarkupTranslationValues,
-  RichTranslationValues,
-  MessageKeys,
-  NamespaceKeys,
-  NestedKeyOf,
-  NestedValueOf,
-} from "next-intl"
-
-type Messages = typeof import("./locales/zh.json")
-declare interface IntlMessages extends Messages {}
-
-export default function useT<
-  NestedKey extends NamespaceKeys<IntlMessages, NestedKeyOf<IntlMessages>> = never,
->(
-  namespace?: NestedKey,
-): {
-  <
-    TargetKey extends MessageKeys<
-      NestedValueOf<
-        {
-          "!": IntlMessages
-        },
-        [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-      >,
-      NestedKeyOf<
-        NestedValueOf<
-          {
-            "!": IntlMessages
-          },
-          [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-        >
-      >
-    >,
-  >(
-    key: TargetKey,
-    values?: TranslationValues,
-    formats?: Formats,
-  ): string
-  rich<
-    TargetKey extends MessageKeys<
-      NestedValueOf<
-        {
-          "!": IntlMessages
-        },
-        [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-      >,
-      NestedKeyOf<
-        NestedValueOf<
-          {
-            "!": IntlMessages
-          },
-          [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-        >
-      >
-    >,
-  >(
-    key: TargetKey,
-    values?: RichTranslationValues,
-    formats?: Formats,
-  ): ReactNode
-  markup<
-    TargetKey extends MessageKeys<
-      NestedValueOf<
-        {
-          "!": IntlMessages
-        },
-        [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-      >,
-      NestedKeyOf<
-        NestedValueOf<
-          {
-            "!": IntlMessages
-          },
-          [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-        >
-      >
-    >,
-  >(
-    key: TargetKey,
-    values?: MarkupTranslationValues,
-    formats?: Formats,
-  ): string
-  raw<
-    TargetKey extends MessageKeys<
-      NestedValueOf<
-        {
-          "!": IntlMessages
-        },
-        [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-      >,
-      NestedKeyOf<
-        NestedValueOf<
-          {
-            "!": IntlMessages
-          },
-          [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-        >
-      >
-    >,
-  >(
-    key: TargetKey,
-  ): any
-  has<
-    TargetKey extends MessageKeys<
-      NestedValueOf<
-        {
-          "!": IntlMessages
-        },
-        [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-      >,
-      NestedKeyOf<
-        NestedValueOf<
-          {
-            "!": IntlMessages
-          },
-          [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
-        >
-      >
-    >,
-  >(
-    key: TargetKey,
-  ): boolean
-}
 
 declare namespace NodeJS {
   interface ProcessEnv {
@@ -147,4 +19,135 @@ declare module "next-auth" {
   interface User {
     role?: string
   }
+}
+
+import {
+  useTranslations,
+  ReactNode,
+  Formats,
+  TranslationValues,
+  MarkupTranslationValues,
+  RichTranslationValues,
+  MessageKeys,
+  NamespaceKeys,
+  NestedKeyOf,
+  NestedValueOf,
+} from "next-intl"
+
+declare module "next-intl" {
+  type Messages = typeof import("./locales/zh.json")
+  declare interface IntlMessages extends Messages {}
+
+  // declare function useTranslations<
+  //   NestedKey extends NamespaceKeys<IntlMessages, NestedKeyOf<IntlMessages>> = never,
+  // >(
+  //   namespace?: NestedKey,
+  // ): {
+  //   <
+  //     TargetKey extends MessageKeys<
+  //       NestedValueOf<
+  //         {
+  //           "!": IntlMessages
+  //         },
+  //         [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //       >,
+  //       NestedKeyOf<
+  //         NestedValueOf<
+  //           {
+  //             "!": IntlMessages
+  //           },
+  //           [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //         >
+  //       >
+  //     >,
+  //   >(
+  //     key: TargetKey,
+  //     values?: TranslationValues,
+  //     formats?: Formats,
+  //   ): string
+  //   rich<
+  //     TargetKey extends MessageKeys<
+  //       NestedValueOf<
+  //         {
+  //           "!": IntlMessages
+  //         },
+  //         [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //       >,
+  //       NestedKeyOf<
+  //         NestedValueOf<
+  //           {
+  //             "!": IntlMessages
+  //           },
+  //           [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //         >
+  //       >
+  //     >,
+  //   >(
+  //     key: TargetKey,
+  //     values?: RichTranslationValues,
+  //     formats?: Formats,
+  //   ): ReactNode
+  //   markup<
+  //     TargetKey extends MessageKeys<
+  //       NestedValueOf<
+  //         {
+  //           "!": IntlMessages
+  //         },
+  //         [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //       >,
+  //       NestedKeyOf<
+  //         NestedValueOf<
+  //           {
+  //             "!": IntlMessages
+  //           },
+  //           [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //         >
+  //       >
+  //     >,
+  //   >(
+  //     key: TargetKey,
+  //     values?: MarkupTranslationValues,
+  //     formats?: Formats,
+  //   ): string
+  //   raw<
+  //     TargetKey extends MessageKeys<
+  //       NestedValueOf<
+  //         {
+  //           "!": IntlMessages
+  //         },
+  //         [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //       >,
+  //       NestedKeyOf<
+  //         NestedValueOf<
+  //           {
+  //             "!": IntlMessages
+  //           },
+  //           [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //         >
+  //       >
+  //     >,
+  //   >(
+  //     key: TargetKey,
+  //   ): any
+  //   has<
+  //     TargetKey extends MessageKeys<
+  //       NestedValueOf<
+  //         {
+  //           "!": IntlMessages
+  //         },
+  //         [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //       >,
+  //       NestedKeyOf<
+  //         NestedValueOf<
+  //           {
+  //             "!": IntlMessages
+  //           },
+  //           [NestedKey] extends [never] ? "!" : `!.${NestedKey}`
+  //         >
+  //       >
+  //     >,
+  //   >(
+  //     key: TargetKey,
+  //   ): boolean
+  // }
 }
