b27583e5805aa379af27486d666f8bab9321b6b6
MiniOcean404 <1037306928@qq.com>
2024-11-26 21:49:51 +0800

    ✨ feat: 导出 blog sql

--- prisma/sql/blog.sql
+++ prisma/sql/blog.sql
@@ -1,15 +1,259 @@
-INSERT INTO `role`
-VALUES
-  ( 1, 0, '超级管理员', 'SPADMIN', 1, 0, '保留，不启用', 1, 1, 0, '2016-07-01 11:30:31', '2022-12-16 23:15:00' );
-INSERT INTO `role`
-VALUES
-  ( 2, 0, '系统管理员', 'SYSADMIN', 2, 1, '管理系统权限资源等后台用户', 1, 1, 0, '2016-07-01 11:30:28', '2021-07-23 17:53:09' );
-INSERT INTO `role`
-VALUES
-  ( 3, 0, '业务管理员', 'BUSINESS_ADMIN', 3, 1, '管理业务相关系统操作', 1, 1, 0, '2016-07-01 11:27:48', '2018-05-27 16:10:08' );
-INSERT INTO `role`
-VALUES
-  ( 4, 0, '测试用户', 'TEST', 4, 1, '管理业务相关系统操作', 1, 1, 0, '2016-07-01 11:27:48', '2022-12-08 11:23:36' );
-INSERT INTO `role`
-VALUES
-  ( 5, 0, '注册用户', 'USER', 4, 1, '管理业务相关系统操作', 1, 1, 0, '2016-07-01 11:27:48', '2020-11-29 19:34:34' );
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 80028 (8.0.28)
+ Source Host           : localhost:3306
+ Source Schema         : blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028 (8.0.28)
+ File Encoding         : 65001
+
+ Date: 26/11/2024 21:48:24
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `userId` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `provider` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `providerAccountId` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `refresh_token` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `access_token` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `expires_at` int NULL DEFAULT NULL,
+  `token_type` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `scope` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `id_token` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `session_state` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `refresh_token_expires_in` int NULL DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider` ASC, `providerAccountId` ASC) USING BTREE,
+  INDEX `Account_userId_idx`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint NULL DEFAULT NULL,
+  `resource_name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_key` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_type` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_level` int NULL DEFAULT NULL,
+  `resource_show` tinyint(1) NULL DEFAULT NULL,
+  `resource_cache` tinyint(1) NULL DEFAULT NULL,
+  `resource_page_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `resource_status` tinyint NULL DEFAULT 1,
+  `comments` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `creator` bigint NULL DEFAULT NULL,
+  `operator` bigint NULL DEFAULT NULL,
+  `del_flag` tinyint NOT NULL DEFAULT 0,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Permission_resource_name_idx`(`resource_name` ASC) USING BTREE,
+  INDEX `Permission_parent_id_idx`(`parent_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint NULL DEFAULT 0,
+  `role_name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `role_key` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `role_level` int NULL DEFAULT NULL,
+  `role_status` tinyint NULL DEFAULT 1,
+  `comments` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `creator` bigint NULL DEFAULT NULL,
+  `operator` bigint NULL DEFAULT NULL,
+  `del_flag` tinyint NULL DEFAULT 0,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `Role_role_key_key`(`role_key` ASC) USING BTREE,
+  INDEX `Role_role_name_idx`(`role_name` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, 0, '超级管理员', 'SPADMIN', 1, 0, '保留，不启用', 1, 1, 0, '2016-07-01 11:30:31.000', '2022-12-16 23:15:00.000');
+INSERT INTO `role` VALUES (2, 0, '系统管理员', 'SYSADMIN', 2, 1, '管理系统权限资源等后台用户', 1, 1, 0, '2016-07-01 11:30:28.000', '2021-07-23 17:53:09.000');
+INSERT INTO `role` VALUES (3, 0, '业务管理员', 'BUSINESS_ADMIN', 3, 1, '管理业务相关系统操作', 1, 1, 0, '2016-07-01 11:27:48.000', '2018-05-27 16:10:08.000');
+INSERT INTO `role` VALUES (4, 0, '测试用户', 'TEST', 4, 1, '管理业务相关系统操作', 1, 1, 0, '2016-07-01 11:27:48.000', '2022-12-08 11:23:36.000');
+INSERT INTO `role` VALUES (5, 0, '注册用户', 'USER', 4, 1, '管理业务相关系统操作', 1, 1, 0, '2016-07-01 11:27:48.000', '2020-11-29 19:34:34.000');
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `role_id` bigint NOT NULL,
+  `permission_id` bigint NOT NULL,
+  `creator` bigint NULL DEFAULT NULL,
+  `operator` bigint NULL DEFAULT NULL,
+  `del_flag` tinyint NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for session
+-- ----------------------------
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session`  (
+  `id` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sessionToken` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userId` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expires` datetime(3) NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `Session_sessionToken_key`(`sessionToken` ASC) USING BTREE,
+  INDEX `Session_userId_idx`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of session
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for token
+-- ----------------------------
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE `token`  (
+  `id` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hashedKey` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `partialKey` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expires` datetime(3) NULL DEFAULT NULL,
+  `lastUsed` datetime(3) NULL DEFAULT NULL,
+  `userId` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rateLimit` int NULL DEFAULT 600,
+  `scopes` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `Token_hashedKey_key`(`hashedKey` ASC) USING BTREE,
+  INDEX `Token_userId_idx`(`userId` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of token
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cuid` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `account` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `nickname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `real_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '2',
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `emailVerified` datetime NULL DEFAULT NULL,
+  `mobile` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `real_assword` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `status` tinyint NULL DEFAULT 1,
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `country` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `province` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `city` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `area` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `street` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `comments` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `creator` bigint NULL DEFAULT NULL,
+  `operator` bigint NULL DEFAULT NULL,
+  `del_flag` tinyint NULL DEFAULT 0,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `User_email_idx`(`email` ASC) USING BTREE,
+  INDEX `User_real_name_idx`(`real_name` ASC) USING BTREE,
+  INDEX `mobile`(`mobile` ASC) USING BTREE,
+  UNIQUE INDEX `User_email_key`(`email` ASC) USING BTREE,
+  UNIQUE INDEX `User_cuid_key`(`cuid` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'cm3ygnao50000jtxtriads19d', NULL, '我是小海洋呀', NULL, '2', '1037306928@qq.com', NULL, NULL, '$2a$10$AvRP7QHGA6h3Ch1b6ywU0OXNBBWLxmyLUn3iq8TZL025atv0DoSgm', '123456', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, '2024-11-26 12:56:07.877', '2024-11-26 12:56:07.877');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  `creator` bigint NULL DEFAULT NULL,
+  `operator` bigint NULL DEFAULT NULL,
+  `del_flag` tinyint NULL DEFAULT 0,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_role_role_id_idx`(`role_id` ASC) USING BTREE,
+  INDEX `user_role_user_id_idx`(`user_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, 1, 5, NULL, NULL, 0, '2024-11-26 12:56:07.890', '2024-11-26 12:56:07.890');
+
+-- ----------------------------
+-- Table structure for verification_token
+-- ----------------------------
+DROP TABLE IF EXISTS `verification_token`;
+CREATE TABLE `verification_token`  (
+  `identifier` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expires` datetime(3) NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `updatedAt` datetime(3) NOT NULL,
+  UNIQUE INDEX `verification_token_token_key`(`token` ASC) USING BTREE,
+  UNIQUE INDEX `verification_token_identifier_token_key`(`identifier` ASC, `token` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of verification_token
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
