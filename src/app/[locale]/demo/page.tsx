d64816fbaaf7ac0f2ceba68089313309a6b4aaf6
MiniOcean404 <1037306928@qq.com>
2024-11-28 00:33:23 +0800

    ✨ feat: code 组件

--- src/app/[locale]/demo/page.tsx
+++ src/app/[locale]/demo/page.tsx
@@ -1,7 +1,12 @@
 "use client"
-
+import Code from "~/lib/components/mini/code"
 import Index from "./index.mdx"
 
 export default function Demo() {
-  return <div className="h-[200vh]">{/* <Index></Index> */}</div>
+  return (
+    <div className="mt-16">
+      <Code code={` console.log("Hello World")`}></Code>
+      <Index></Index>
+    </div>
+  )
 }
