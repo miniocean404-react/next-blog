d9d655ff496938854cecc7f3a8bbd698e2c2e27a
MiniOcean404 <1037306928@qq.com>
2024-12-01 00:34:45 +0800

    ✨ feat: 类型声明问题

--- global.d.ts
+++ global.d.ts
@@ -1,34 +1,34 @@
 declare global {
-  declare namespace NodeJS {
-    declare interface ProcessEnv {
-      // 基础路径
-      NEXT_PUBLIC_BASEURL: string
-
-      AUTH_SECRET: string
-
-      AUTH_GITHUB_ID: string
-      AUTH_GITHUB_SECRET: string
-
-      AUTH_GOOGLE_ID: string
-      AUTH_GOOGLE_SECRET: string
-
-      DOUBAO_API_KEY: string
-
-      IMAGE_KIT_PUBLIC_KEY: string
-      IMAGE_KIT_PRIVATE_KEY: string
-      IMAGE_KIT_URL_ENDPOINT: string
-
-      // 数据库
-      DATABASE_URL: string
-    }
-  }
-
-  type Messages = typeof import("./locales/zh.json")
-  declare interface IntlMessages extends Messages {}
-
   declare module "next-auth" {
     interface User {
       role?: string | null
     }
+  }
+}
+
+type Messages = typeof import("./locales/zh.json")
+declare interface IntlMessages extends Messages {}
+
+declare namespace NodeJS {
+  declare interface ProcessEnv {
+    // 基础路径
+    NEXT_PUBLIC_BASEURL: string
+
+    AUTH_SECRET: string
+
+    AUTH_GITHUB_ID: string
+    AUTH_GITHUB_SECRET: string
+
+    AUTH_GOOGLE_ID: string
+    AUTH_GOOGLE_SECRET: string
+
+    DOUBAO_API_KEY: string
+
+    IMAGE_KIT_PUBLIC_KEY: string
+    IMAGE_KIT_PRIVATE_KEY: string
+    IMAGE_KIT_URL_ENDPOINT: string
+
+    // 数据库
+    DATABASE_URL: string
   }
 }
