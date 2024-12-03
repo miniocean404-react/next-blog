750f1713c18beb21c6a581ba37c830a48134a3dd
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-12-03 16:12:38 +0800

    ✨ feat: mdx

--- src/app/[locale]/demo/page1.tsx
+++ src/app/[locale]/demo/page1.tsx
@@ -1,11 +1,9 @@
 import Code from "~/lib/components/mini/code"
-import Index from "./index.mdx"
 
 export default function Demo() {
   return (
     <div className="mt-16">
       <Code code={` console.log("Hello World")`}></Code>
-      <Index></Index>
     </div>
   )
 }
