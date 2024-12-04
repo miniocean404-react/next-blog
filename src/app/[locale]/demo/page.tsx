36852f068bb9e6de748548c0386ff2e157aa6392
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-12-04 15:01:07 +0800

    ✨ feat: mdx 布局

--- src/app/[locale]/demo/page.tsx
+++ src/app/[locale]/demo/page.tsx
@@ -1,15 +1,29 @@
+import { cn } from "@/utils/tw"
 import { allDocs } from "contentlayer/generated"
 import { Mdx } from "~/content/components"
-import { useLiveReload } from "next-contentlayer2/hooks"
+// import { useLiveReload } from "next-contentlayer2/hooks"
 
 export default function Demo() {
   return (
-    <div className="mt-16 mx-auto max-w-mini-layout ">
-      <ul>
+    <div
+      className={cn(
+        "w-full mt-24 mx-auto py-6 grid px-8",
+        "lg:grid-cols-[240px_minmax(0,1fr)_300px] lg:gap-10",
+        "md:grid-cols-[220px_minmax(0,1fr)_300px] md:gap-6",
+      )}
+    >
+      <aside></aside>
+
+      <main>
         {allDocs.map((doc) => {
+          // console.log(doc.slug)
+          // console.log(doc.slugAsParams)
+
           return <Mdx key={doc._id} code={doc.body.code}></Mdx>
         })}
-      </ul>
+      </main>
+
+      <div></div>
     </div>
   )
 }
