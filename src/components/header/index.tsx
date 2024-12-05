cde3da31892b78427b81f39160cc498ef45f28bd
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-12-05 16:40:58 +0800

    ♻️ refactor: header 并添加登录注册页面没有 header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,6 @@
 
 import AlgoliaSearch from "../algolia-search"
 import ThemeSwitch from "./theme-switch"
-import DynWebTitle from "./dyn-web-title"
 
 import Link from "next/link"
 import Image from "next/image"
@@ -12,54 +11,44 @@ import { getLocale, getTranslations } from "next-intl/server"
 import { Account } from "./sign-up"
 import { Navigation } from "@/components/header/navigation"
 import HeaderLayout from "@/components/header/layout"
-import RainbowBackground from "~/lib/components/mini/rainbow"
-import HeaderShow from "@/components/header/show"
 
 export default async function Header() {
   // const header = await headers()
   // const _ = header.get("user-agent")
-  const t = await getTranslations()
+  const t = await getTranslations("app")
   const locale = await getLocale()
   const session = await auth()
 
   return (
-    <HeaderShow>
-      {/* 彩虹 */}
-      <RainbowBackground />
+    <HeaderLayout>
+      <Link
+        className="pl-4 text-base font-600 flex items-center"
+        href={`/${locale}`}
+        title={locale}
+      >
+        <Image
+          className="mr-2 h-6"
+          src={"/favicon.svg"}
+          alt={"logo"}
+          width={24}
+          height={24}
+          priority
+        />
+        <span>{t("blogName")}</span>
+      </Link>
 
-      {/* 网站标题图标切换 */}
-      <DynWebTitle title={t("app.blogName")}></DynWebTitle>
-
-      <HeaderLayout>
-        <Link
-          className="pl-4 text-base font-600 flex items-center"
-          href={`/${locale}`}
-          title={locale}
-        >
-          <Image
-            className="mr-2 h-6"
-            src={"/favicon.svg"}
-            alt={"logo"}
-            width={24}
-            height={24}
-            priority
-          />
-          <span>{t("app.blogName")}</span>
-        </Link>
-
-        <div className="flex items-center md:flex-grow">
-          <div className="md:pl-6 md:flex md:flex-grow">
-            <AlgoliaSearch />
-          </div>
-
-          <Navigation />
-
-          <div className="hidden md:items-center md:flex">
-            <ThemeSwitch />
-            <Account session={session}></Account>
-          </div>
+      <div className="flex items-center md:flex-grow">
+        <div className="md:pl-6 md:flex md:flex-grow">
+          <AlgoliaSearch />
         </div>
-      </HeaderLayout>
-    </HeaderShow>
+
+        <Navigation />
+
+        <div className="hidden md:items-center md:flex">
+          <ThemeSwitch />
+          <Account session={session}></Account>
+        </div>
+      </div>
+    </HeaderLayout>
   )
 }
--- src/components/header/layout.tsx
+++ src/components/header/layout.tsx
@@ -1,17 +1,35 @@
+"use client"
+
+import DynWebTitle from "@/components/header/dyn-web-title"
 import { cn } from "@/utils/tw"
+import { useTranslations } from "next-intl"
+import { usePathname } from "next/navigation"
 import type { PropsWithChildren } from "react"
+import RainbowBackground from "~/lib/components/mini/rainbow"
 
 export default function HeaderLayout({ children }: PropsWithChildren<any>) {
+  const pathname = usePathname()
+  if (pathname.includes("passport")) return null
+  const t = useTranslations("app")
+
   return (
-    <header
-      className={cn(
-        "sticky top-0 z-mini-header backdrop-blur-md",
-        "border-b",
-        "flex items-center h-16 w-full",
-        "px-2 md:px-8",
-      )}
-    >
-      <div className="flex w-full max-w-mini-layout mx-auto justify-between">{children}</div>
-    </header>
+    <>
+      {/* 网站标题图标切换 */}
+      <DynWebTitle title={t("blogName")}></DynWebTitle>
+
+      {/* 彩虹 */}
+      <RainbowBackground />
+
+      <header
+        className={cn(
+          "sticky top-0 z-mini-header backdrop-blur-md",
+          "border-b",
+          "flex items-center h-16 w-full",
+          "px-2 md:px-8",
+        )}
+      >
+        <div className="flex w-full max-w-mini-layout mx-auto justify-between">{children}</div>
+      </header>
+    </>
   )
 }
