1fc9d746b8236b7ee29a47c67ca45c96acd93553
MiniOcean404 <1037306928@qq.com>
2024-12-15 23:00:05 +0800

    ♻️ refactor: 优化漂亮文字的央视

--- lib/components/mini/beautiful-text/index.tsx
+++ lib/components/mini/beautiful-text/index.tsx
@@ -9,70 +9,57 @@ interface BeautifulTextProps {
 
 export default function BeautifulText(props: PropsWithChildren<BeautifulTextProps>) {
   return (
-    <span className={cn("relative inline-flex overflow-hidden", props.className)}>
+    <span className={cn("relative inline-flex", "overflow-hidden", props.className)}>
       {props.text}
 
-      <div className="aurora pointer-events-none absolute inset-0 mix-blend-lighten dark:mix-blend-darken">
+      <div
+        className={cn(
+          "aurora pointer-events-none absolute inset-0",
+          "mix-blend-lighten dark:mix-blend-darken",
+        )}
+      >
         <div
           style={{
             backgroundColor: "hsl(var(--mini-aurora-color-1))",
-            filter: "blur(1rem)",
             animation:
               "mini-aurora-border 6s ease-in-out infinite, mini-aurora-1 12s ease-in-out infinite alternate",
-            mixBlendMode: "overlay",
-            top: "-50%",
           }}
-          className="absolute h-[60vw] w-[60vw]"
+          className={cn("absolute h-[60vw] w-[60vw] blur-lg", "mix-blend-overlay")}
         ></div>
 
         <div
           style={{
             backgroundColor: "hsl(var(--mini-aurora-color-2))",
-            filter: "blur(1rem)",
             animation:
               "mini-aurora-border 6s ease-in-out infinite, mini-aurora-2 12s ease-in-out infinite alternate",
-            mixBlendMode: "overlay",
-            right: "0",
-            top: "0",
           }}
-          className="absolute h-[60vw] w-[60vw]"
+          className={cn("absolute h-[60vw] w-[60vw] blur-lg", "mix-blend-overlay")}
         ></div>
 
         <div
           style={{
             backgroundColor: "hsl(var(--mini-aurora-color-3))",
-            filter: "blur(1rem)",
             animation:
               "mini-aurora-border 6s ease-in-out infinite, mini-aurora-3 12s ease-in-out infinite alternate",
-            mixBlendMode: "overlay",
-            left: "0",
-            bottom: "0",
           }}
-          className="absolute h-[60vw] w-[60vw]"
+          className={cn("absolute h-[60vw] w-[60vw] blur-lg", "mix-blend-overlay")}
         ></div>
 
         <div
           style={{
             backgroundColor: "hsl(var(--mini-aurora-color-4))",
-            filter: "blur(1rem)",
             animation:
               "mini-aurora-border 6s ease-in-out infinite, mini-aurora-4 12s ease-in-out infinite alternate",
-            mixBlendMode: "overlay",
-            right: "0",
-            bottom: "-50%",
           }}
-          className="absolute h-[60vw] w-[60vw]"
+          className={cn("absolute h-[60vw] w-[60vw] blur-lg", "mix-blend-overlay")}
         ></div>
 
         <div
           style={{
             backgroundColor: "hsl(var(--mini-aurora-color-5))",
-            filter: "blur(1rem)",
-            animation:
-              "mini-aurora-border 6s ease-in-out infinite, mini-aurora-5 12s ease-in-out infinite alternate",
-            mixBlendMode: "overlay",
+            animation: "mini-aurora-border 6s ease-in-out infinite",
           }}
-          className="absolute h-[60vw] w-[60vw]"
+          className={cn("absolute h-[60vw] w-[60vw] blur-lg", "mix-blend-overlay")}
         ></div>
       </div>
     </span>
