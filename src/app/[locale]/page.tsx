115b473231346fa8df977e0188f2b5c06738e210
MiniOcean404 <1037306928@qq.com>
2024-12-15 21:41:48 +0800

    💄 style: 删除无用代码

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,7 @@
 import FloatDock from "@/components/float-docker"
 import FrameworkIcon from "@/components/home-sections/framework-icon"
 import Slogan from "@/components/home-sections/slogan"
-import ParticlesBackground from "@/components/particles-background"
 import Scroll from "@/components/scroll"
-import TypedJs from "@/components/typed"
 import { getTranslations } from "next-intl/server"
 
 export default async function Home(props: { params: { locale: string } }) {
@@ -11,11 +9,6 @@ export default async function Home(props: { params: { locale: string } }) {
 
   return (
     <div>
-      {/* <h1 className="hidden">{t("app.appDefaultTitle")}</h1> */}
-      {/* <TypedJs texts={[`<span>${t("home.slogan.first")}</span>`, t("home.slogan.second")]} /> */}
-      {/* 导致电脑太卡，生产再打开 */}
-      {process.env.NODE_ENV === "production" && <ParticlesBackground />}
-
       <Slogan />
       <FrameworkIcon />
       <FloatDock></FloatDock>
