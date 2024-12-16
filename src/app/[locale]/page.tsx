78e78bca3db894e2ce9e2a5537531c2735fe7e67
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-12-16 11:00:26 +0800

    🐛 fix: 修复首页技术栈 title 层级

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
-import FloatDock from "@/components/float-docker"
+import FloatDock from "@/components/home-sections/float-dock"
 import FrameworkIcon from "@/components/home-sections/framework-icon"
 import Slogan from "@/components/home-sections/slogan"
-import Scroll from "@/components/scroll"
+import Scroll from "@/components/home-sections/scroll"
 import { getTranslations } from "next-intl/server"
 
 export default async function Home(props: { params: { locale: string } }) {
