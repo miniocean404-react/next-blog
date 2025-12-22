2b3b1579704364fdab56938463273a9c51d3e142
MiniOcean404 <1037306928@qq.com>
2025-12-23 01:14:29 +0800

    ♻️ refactor: 升级问题处理

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ export default function HomeLayout({ children }: LayoutPropsWith<LayoutParams>) 
   return (
     // 为 vaul 抽屉组件包裹一层
     <div className="bg-background" data-vaul-drawer-wrapper>
-      <Header></Header>
+      <Header />
       <main>{children}</main>
     </div>
   )
